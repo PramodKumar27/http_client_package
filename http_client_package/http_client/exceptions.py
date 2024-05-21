95333c6f3423d8dab54219b64efb6e16180e3d89
PramodKumar27 <136595417+PramodKumar27@users.noreply.github.com>
2024-05-21 15:18:10 +0530

    Delete http_client_package directory

--- http_client_package/http_client/exceptions.py
+++
@@ -1,9 +0,0 @@
-class HTTPRequestException(Exception):
-    def __init__(self, message):
-        super().__init__(message)
-
-
-class ValidationError(Exception):
-    def __init__(self, message):
-        self.message = message
-        super().__init__(self.message)
